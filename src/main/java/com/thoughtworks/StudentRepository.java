f838be57ad2c0a52ac33a256d1306846740dd2aa
zkj <438101845@qq.com>
2020-03-27 11:08:49 +0800

    {zkj}-course4:modify sqlException

--- src/main/java/com/thoughtworks/StudentRepository.java
+++ src/main/java/com/thoughtworks/StudentRepository.java
@@ -7,89 +7,124 @@ import java.util.List;
 public class StudentRepository {
 
   public void save(List<Student> students) {
-      students.forEach(student -> {
-        try {
-          this.save(student);
-        } catch (SQLException e) {
-          e.printStackTrace();
-        }
-      });
+      students.forEach(this::save);
   }
 
-  public void save(Student student) throws SQLException {
+  public void save(Student student){
     // TODO:
-    Connection conn = DbUtil.getConnection();
     String sql = "INSERT INTO student(id, name, sex, admission_year, birthday, classroom)"
             + "values(" + "?,?,?,?,?,?)";
-    PreparedStatement ptmt = conn.prepareStatement(sql);
-    addData(ptmt, student);
+    Connection conn;
+    PreparedStatement ptmt;
+    try {
+      conn = DbUtil.getConnection();
+      ptmt = conn.prepareStatement(sql);
+      addData(ptmt, student);
 
-    ptmt.executeLargeUpdate();
+      ptmt.executeLargeUpdate();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 
-  public List<Student> query() throws SQLException {
+  public List<Student> query(){
     // TODO:
-    Connection conn = DbUtil.getConnection();
-    Statement stmt = conn.createStatement();
-    ResultSet rs = stmt.executeQuery("SELECT id, name, sex, admission_year, birthday, classroom FROM student");
+    Connection conn;
+    Statement stmt;
+    ResultSet rs;
+    try {
+      conn = DbUtil.getConnection();
+      stmt = conn.createStatement();
+      rs = stmt.executeQuery("SELECT id, name, sex, admission_year, birthday, classroom FROM student");
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
     return addToListFromResultSet(rs);
   }
 
-  public List<Student> queryByClassId(String classId) throws SQLException {
+  public List<Student> queryByClassId(String classId) {
     // TODO:
-    Connection conn = DbUtil.getConnection();
     String sql = "SELECT id, name, sex, admission_year, birthday, classroom FROM student " +
             "WHERE classroom = ? ORDER BY id DESC";
-    PreparedStatement ptmt = conn.prepareStatement(sql);
-    ptmt.setString(1, classId);
-    ResultSet rs = ptmt.executeQuery();
-    return addToListFromResultSet(rs);
+    Connection conn;
+    PreparedStatement ptmt;
+    try {
+      conn = DbUtil.getConnection();
+      ptmt = conn.prepareStatement(sql);
+      ptmt.setString(1, classId);
+      ResultSet rs = ptmt.executeQuery();
+      return addToListFromResultSet(rs);
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 
-  public void update(String id, Student student) throws SQLException {
+  public void update(String id, Student student) {
     // TODO:
-    Connection conn = DbUtil.getConnection();
     String sql = "UPDATE student SET id = ?,name = ?,sex = ?,admission_year = ?,birthday = ?,classroom = ? WHERE id = ?";
-    PreparedStatement preparedStatement = conn.prepareStatement(sql);
-    addData(preparedStatement, student);
-    preparedStatement.setString(7, id);
+    Connection conn;
+    PreparedStatement ptmt;
+    try {
+      conn = DbUtil.getConnection();
+      ptmt = conn.prepareStatement(sql);
+      addData(ptmt, student);
+      ptmt.setString(7, id);
 
-    preparedStatement.executeUpdate();
+      ptmt.executeUpdate();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+
   }
 
-  public void delete(String id) throws SQLException {
+  public void delete(String id) {
     // TODO:
-    Connection conn = DbUtil.getConnection();
-    String sql = "DELETE FROM student WHERE id = ?";
-    PreparedStatement preparedStatement = conn.prepareStatement(sql);
+    Connection conn;
+    PreparedStatement ptmt;
+    try {
+      conn = DbUtil.getConnection();
+      String sql = "DELETE FROM student WHERE id = ?";
+      ptmt = conn.prepareStatement(sql);
 
-    preparedStatement.setString(1, id);
-    preparedStatement.executeUpdate();
+      ptmt.setString(1, id);
+      ptmt.executeUpdate();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 
-  public List<Student> addToListFromResultSet(ResultSet rs) throws SQLException {
+  public List<Student> addToListFromResultSet(ResultSet rs) {
     List<Student> students = new ArrayList<>();
     Student s;
-    while (rs.next()) {
-      s = new Student();
-      s.setId(rs.getString("id"));
-      s.setName(rs.getString("name"));
-      s.setGender(rs.getString("sex"));
-      s.setAdmissionYear(rs.getInt("admission_year"));
-      s.setBirthday(rs.getDate("birthday"));
-      s.setClassId(rs.getString("classroom"));
+    try {
+      while (rs.next()) {
+        s = new Student();
+        s.setId(rs.getString("id"));
+        s.setName(rs.getString("name"));
+        s.setGender(rs.getString("sex"));
+        s.setAdmissionYear(rs.getInt("admission_year"));
+        s.setBirthday(rs.getDate("birthday"));
+        s.setClassId(rs.getString("classroom"));
 
-      students.add(s);
+        students.add(s);
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
     return students;
   }
 
-  public void addData(PreparedStatement preparedStatement, Student student) throws SQLException {
-    preparedStatement.setString(1, student.getId());
-    preparedStatement.setString(2, student.getName());
-    preparedStatement.setString(3, student.getGender());
-    preparedStatement.setInt(4, student.getAdmissionYear());
-    preparedStatement.setDate(5, new Date(student.getBirthday().getTime()));
-    preparedStatement.setString(6, student.getClassId());
+  public void addData(PreparedStatement preparedStatement, Student student) {
+    try  {
+      preparedStatement.setString(1, student.getId());
+      preparedStatement.setString(2, student.getName());
+      preparedStatement.setString(3, student.getGender());
+      preparedStatement.setInt(4, student.getAdmissionYear());
+      preparedStatement.setDate(5, new Date(student.getBirthday().getTime()));
+      preparedStatement.setString(6, student.getClassId());
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
